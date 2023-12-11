7180079bdcd96141f1a315231341824ec029ffe7
BHALA2004 <balakannantvl@gmail.com>
2023-12-11 12:00:00 +0530

    recur

---
+++ src/recursive/importantRecursiveProblem/FormNumber.java
@@ -0,0 +1,25 @@
+package recursive.importantRecursiveProblem;
+
+public class FormNumber {
+    public static void main(String[] args) {
+        formNumber("",4);
+        formNumber("",4,2);
+    }
+    public static void formNumber(String p,int target,int face){
+        if(target==0){
+            System.out.println(p);return;
+        }
+        for(int i=1;i<=face && i<=target;i++){
+            formNumber(p+i,target-i,face);
+        }
+    }
+
+    public static void formNumber(String p,int target){
+        if(target==0){
+            System.out.println(p);return;
+        }
+        for(int i=1;i<=6 && i<=target;i++){
+            formNumber(p+i,target-i);
+        }
+    }
+}
