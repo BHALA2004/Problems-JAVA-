8938f3f2298e71970c0b4f9053e33631081e9418
BHALA2004 <balakannantvl@gmail.com>
2024-10-28 23:01:58 +0530

    new

---
+++ src/tcs/SnakePattern.java
@@ -0,0 +1,24 @@
+package tcs;
+
+public class SnakePattern {
+    public static void main(String[] args) {
+        int count = 0;int flag = 0;int temp=0;int n = 5;
+        for(int i = 1 ;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                if(i%2==1){
+                    count+=1;
+                   System.out.print(count + " ");
+                   flag=0;
+
+               }
+                else {
+                    if(flag==0){
+                    count=i*n;flag=1;temp = count;}
+                    System.out.print(temp +" ");
+                    temp-=1;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
