75347ac7b43edf52be43188bfa86614f5506b127
BHALA2004 <balakannantvl@gmail.com>
2024-10-29 21:38:02 +0530

    new

--- src/ListFunctions/LinkedListMain.java
+++ src/ListFunctions/LinkedListMain.java
@@ -1,16 +1,25 @@
 package ListFunctions;// Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
+import java.util.ArrayList;
+import java.util.List;
+
 class LinkedListMain{
     public static void main(String[] args){
         LinkedList ll = new LinkedList();
 
         ll.insertEnd(1);
-        ll.insertEnd(1);
         ll.insertEnd(2);
         ll.insertEnd(3);
-        ll.insertEnd(3);
-        ll.deletedupli();
+        ll.insertEnd(4);
+        ll.insertEnd(1);
+        ll.insertEnd(2);
+
+        ll.rotate();
+        ll.middle();
+        System.out.println("Answer"+ll.pairSum());
+        ll.display();
+
 
     }
 }
@@ -49,6 +58,13 @@ class LinkedList{
         temp.next=null;
         tail=temp;
     }
+    public void display(Node head){
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
     public void display(){
         Node temp=head;
         while(temp!=null){
@@ -69,7 +85,7 @@ class LinkedList{
                 ptr2=ptr2.next;
             }
         }
-        display();
+
 
     }
     public boolean search(int data){
@@ -82,4 +98,80 @@ class LinkedList{
         }
         return false;
     }
+    public  int size(){
+        Node temp = head;int count = 0;
+        while (temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        return count;
+    }
+public void rotate(){
+    Node temp = head;
+    for(int i = 1;i<size()-1;i++){
+        temp=temp.next;
+    }
+    Node lastNode = temp.next;
+    temp.next=null;
+    lastNode.next=head;
+    head=lastNode;
+
+}
+public void middle(){
+        int m = size()/2;
+        Node temp = head;
+        for(int i = 1;i<m;i++){
+            temp=temp.next;
+        }
+    temp.next=temp.next.next;
+
+}
+public int max(ArrayList<Integer> list){
+        int m = Integer.MIN_VALUE;int max = 0;
+        for(int i : list){
+            m=Math.max(m,i);
+        }
+        return m;
+}
+    public int pairSum() {
+        ArrayList<Integer> list = new ArrayList<>();
+        int m = size()/2;
+        Node temp = head;
+        for(int i = 1;i<m;i++){
+            temp=temp.next;
+        }
+        Node head1 = temp.next;
+        temp.next=null;
+        display(head);
+        System.out.println("Node 2");
+        display(head1);
+        Node heads = reverse(head1);
+        System.out.println("reverse");
+        display(heads);
+        while (head!=null && heads!=null){
+            int sum = head.data+heads.data;
+            list.add(sum);
+            head=head.next;
+            heads=heads.next;
+        }
+        System.out.println(list);
+        return max(list);
+    }
+    public Node reverse(Node head){
+        Node prev = null;
+        Node present = head;
+        Node future = head.next;
+        while (present!=null){
+            present.next=prev;
+            prev=present;
+            present=future;
+            if(future!=null){
+                future=future.next;
+            }
+
+        }
+        return head=prev;
+    }
+
+
 }
