239306c1efe1b7997e0cdcda7ce5c0e4a2e88149
BALASUBRAMANIAN K <138891732+BHALA2004@users.noreply.github.com>
2025-02-05 09:00:20 +0530

    Update AddTwoNumbers

--- src/ListFunctions/AddTwoNumbers.java
+++ src/ListFunctions/AddTwoNumbers.java
@@ -1,13 +1,3 @@
-/******************************************************************************
-
- Online Java Compiler.
- Code, Compile, Run and Debug java program online.
- Write your code in this editor and press "Run" button to execute it.
-
- *******************************************************************************/
-
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 
 class AddTwoNumbers {
 
